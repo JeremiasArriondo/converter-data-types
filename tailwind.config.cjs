85987314ba354328f4c4c559e59d8c9368296b5b
Jeremias Arriondo <jeremiasarriondo98@gmail.com>
2023-08-17 01:38:30 -0300

    style: change base colors

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,9 +4,9 @@ module.exports = {
 	theme: {
 		extend: {
 			colors: {
-				base: "#883aeb",
-        		"base-light": "#e0ccfa",
-        		"base-dark": "#310a65",
+				base: "#2563eb",
+        		"base-light": "#93c5fd",
+        		"base-dark": "#009FE0",
 			}
 		},
 	},
