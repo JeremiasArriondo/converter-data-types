9504e972d39ee9cf2af17707f38b6df695b52488
Jeremias Arriondo <jeremiasarriondo98@gmail.com>
2023-08-18 00:52:22 -0300

    feat: added types ts

--- src/components/Converter.tsx
+++ src/components/Converter.tsx
@@ -1,118 +1,37 @@
+import { h } from "preact";
 import { useState } from "preact/hooks";
+import { converter, JSONSchema } from "src/utils/converter";
+import { generateRamlDataType } from "src/utils/generateRamlDataType";
 
 export default function Converter() {
-  const json = JSON.stringify({
-    test: "prueba",
-    numero: 1,
-    camion: ["uno", "dos", "tres"],
-    pruebaObjeto: {
-      key1: "string",
-      key2: 1,
-      key3: {
-        primer: "string",
-      },
-    },
-    test1: "prueba",
-    numero1: 1,
-    camion1: ["uno", "dos", "tres"],
-    auto: {
-      conductor: {
-        nombre: "Jeremias",
-      },
-    },
-    pruebaObjeto1: {
-      key1: "string",
-      key2: 1,
-    },
-    test2: "prueba",
-    numero2: 1,
-    camion2: ["uno", "dos", "tres"],
-    pruebaObjeto2: {
-      key1: "string",
-      key2: 1,
-    },
-    auto2: {
-      conductor: {
-        nombre: "Jeremias",
-      },
-    },
-  });
-
-  const [input, setInput] = useState("");
-  const [result, setResult] = useState("");
+  const [textArea, setTextArea] = useState("");
+  const [result, setResult] = useState<JSONSchema>(null);
   const [error, setError] = useState(null);
 
-  const onHandler = (e) => {
-    setInput(e.target.value);
+  const onHandlerTextArea = (e: h.JSX.TargetedEvent<HTMLTextAreaElement>) => {
+    setTextArea(e.currentTarget.value);
   };
 
-  const onHandlerConverter = (value) => {
+  const onHandlerConverter = (value: string) => {
     try {
       const json = JSON.parse(value);
-      setResult(converter(json));
+      const prevResult = converter(json);
+      setResult(prevResult);
       setError(null);
     } catch (error) {
       setError(error.name + error.message);
     }
   };
 
-  const converter = (json) => {
-    if (typeof json !== "object" || json === null) {
-      return { type: typeof json };
-    }
-
-    if (Array.isArray(json)) {
-      return {
-        type: "array",
-        items: converter(json[0]),
-      };
-    }
-
-    const properties = {};
-    for (const key in json) {
-      if (json.hasOwnProperty(key)) {
-        properties[key] = converter(json[key]);
-      }
-    }
-
-    return {
-      type: "object",
-      properties,
-    };
-  };
-
-  const generateRamlDataType = (obj, level = 0, isFirstLevel = true) => {
-    let result = isFirstLevel ? "#%RAML 1.0 DataType\n\n" : "";
-
-    for (const [key, value] of Object.entries(obj)) {
-      if (value === "vacio") {
-        result += `${"  ".repeat(level)}${key}: string\n`;
-      } else if (typeof value === "object") {
-        result += `${"  ".repeat(level)}${key}:\n`;
-        result += generateRamlDataType(value, level + 1, false);
-      } else {
-        result += `${"  ".repeat(level)}${key}: ${value}\n`;
-      }
-    }
-
-    return result;
-  };
-
-  const jsonExample = JSON.stringify({
-    example: {
-      tool: "Mulesoft",
-    },
-  });
-
   return (
-    <div>
+    <>
       <div class="flex justify-between">
         <button>Pegar JSON</button>
         <button
-          onClick={() => onHandlerConverter(input)}
-          disabled={input === ""}
+          onClick={() => onHandlerConverter(textArea)}
+          disabled={textArea === ""}
           className={`${
-            input === ""
+            textArea === ""
               ? "disabled:opacity-50 pointer-events-none text-gray-400"
               : "text-white"
           } block `}
@@ -123,9 +42,9 @@ export default function Converter() {
       </div>
       <div className="grid grid-cols-2 h-[calc(100vh-20rem)] overflow-auto">
         <textarea
-          onInput={onHandler}
-          value={input}
-          placeholder={jsonExample}
+          onInput={onHandlerTextArea}
+          value={textArea}
+          placeholder={`{"example":{"tool":"Mulesoft"}`}
           className="h-full px-4 block text-base text-slate-800 overflow-y-auto resize-none"
         />
         <div className="bg-blue-950/50 p-4">
@@ -141,6 +60,6 @@ export default function Converter() {
           <p className="text-base text-white bg-red-500 p-2">{error}</p>
         )}
       </div>
-    </div>
+    </>
   );
 }
